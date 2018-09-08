8fc2ef8802f445cff3c54fe4d1791a8c2ef530f1
Joshua Herron <herronjo110@live.com>
2018-09-07 22:09:03 -0400

    Create passwd.js

---
+++ js/passwd.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+  var sess = window.localStorage.getItem("sess");
+  document.getElementById("submit").onclick = function(evt) {
+    document.getElementById("badopass").style.display = "none";
+    document.getElementById("badmatch").style.display = "none";
+    var opass = document.getElementById("opass").value;
+    var npass = document.getElementById("npass").value;
+    var npass2 = document.getElementById("npass2").value;
+    if (npass == npass2 && npass != "" && npass != undefined) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("https://api.stibarc.gq/updatepasswd.sjs",true);
+      xhr.send("sess="+sess+"&old="+encodeURIComponent(opass)+"&new="+encodeURICompontent(npass)+"&new2="+encodeURICompontent(npass));
+      xhr.onload = function(e) {
+        if (xhr.responseText.split("\n")[0] == "Updated") {
+          location.href = "/";
+        } else {
+          document.getElementById("badopass").style.display = "";
+        }
+      }
+    } else {
+      document.getElementById("badmatch").style.display = "";
+    }
+  }
+}
